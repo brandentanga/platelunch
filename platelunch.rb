dda8684eb42429e7c30ecb99dba4a42bbc7f2c58
Branden Tanga <branden.tanga@gmail.com>
2014-08-25 00:22:09 -0700

    first commit

---
+++ platelunch.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+raw_data = `git log --since=1.day --shortstat | grep "files changed"`
+@insertions, @deletions, @commits = 0, 0, 0
+
+def stats_for_today(raw_data)
+  raw_data.scan(/changed, (\d+)/).each do |num|
+    @insertions += num[0].to_i
+    @commits += 1
+  end
+  raw_data.scan(/(\d+) deletions/).each do |num|
+    @deletions += num[0].to_i
+  end
+  puts "stats for today"
+  puts "number of commits: #{@commits}"
+  puts "number of insertions: #{@insertions}"
+  puts "number of deletions: #{@deletions}"
+end
+
+case ARGV[0]
+when "stats_for_today"
+  puts raw_data
+  stats_for_today raw_data
+when "raw_data"
+  puts raw_data
+else
+  puts "Command not recognized. No plate lunch for you."
+  puts "Usage: stats_for_today raw_data"
+end
